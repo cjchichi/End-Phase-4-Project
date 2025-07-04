e86665fd280a2bd8e4970d1a5e6ff42f058cb7ef
cjchichi <cynthia1973chichi@gmail.com>
2025-07-04 15:53:39 +0300

    login

--- study-group-frontend/src/pages/LoginPage.js
+++ study-group-frontend/src/pages/LoginPage.js
@@ -281,7 +281,6 @@ export default function LoginPage() {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
-          'Authorization': `Bearer ${token}`
         },
         body: JSON.stringify({
           username: formData.username,
